694322a3ec44b2a6b5ccaaff26aae62bd9022020
heisenberg15 <txelidzemaka@yahoo.com>
2018-04-23 15:24:07 +0400

    Save activity state.
    Save activity visibility state on rotate,
    added empty settings activity, menu options in toolbar.

--- app/src/main/java/com/example/fergie/timetable/Fragments/CreateSubjFragment.java
+++ app/src/main/java/com/example/fergie/timetable/Fragments/CreateSubjFragment.java
@@ -80,6 +80,8 @@ public class CreateSubjFragment extends Fragment
         });
     }
 
+
+
 }
 
 
--- app/src/main/java/com/example/fergie/timetable/MainActivity.java
+++ app/src/main/java/com/example/fergie/timetable/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.fergie.timetable;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
@@ -11,6 +12,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -102,99 +105,51 @@ public class MainActivity extends AppCompatActivity implements Communicator
         editor.apply();
     }
 
-    // retry saved subject lists in sharedPreferences for each day
-//    private void retrySharedPreferences()
-//    {
-//        SharedPreferences saveSingleton = getSharedPreferences("saveSingleton", MODE_PRIVATE);
-//        String mondayList = saveSingleton.getString("mondayList", null);
-//        String tuesdayList = saveSingleton.getString("tuesdayList", null);
-//        String wednesdayList = saveSingleton.getString("wednesdayList", null);
-//        String thursdayList = saveSingleton.getString("thursdayList", null);
-//        String fridayList = saveSingleton.getString("fridayList", null);
-//        String saturdayList = saveSingleton.getString("saturdayList", null);
-//        String sundayList = saveSingleton.getString("sundayList", null);
-//
-//        if (mondayList != null) {
-//            ArrayList<SubjectModel> getMondayList = new Gson().fromJson(mondayList, new TypeToken<ArrayList<SubjectModel>>()
-//            {
-//            }.getType());
-//
-//            for (int i = 0; i < getMondayList.size(); i++) {
-//                Singleton.getInstance().addMonSubject(getMondayList.get(i));
-//            }
-//        }
-//
-//        if (tuesdayList != null) {
-//            ArrayList<SubjectModel> getTuesdayList = new Gson().fromJson(tuesdayList, new TypeToken<ArrayList<SubjectModel>>()
-//            {
-//            }.getType());
-//
-//            for (int i = 0; i < getTuesdayList.size(); i++) {
-//                Singleton.getInstance().addTueSubject(getTuesdayList.get(i));
-//            }
-//        }
-//
-//        if (wednesdayList != null) {
-//            ArrayList<SubjectModel> getWednesdayList = new Gson().fromJson(wednesdayList, new TypeToken<ArrayList<SubjectModel>>()
-//            {
-//            }.getType());
-//
-//            for (int i = 0; i < getWednesdayList.size(); i++) {
-//                Singleton.getInstance().addWedSubject(getWednesdayList.get(i));
-//            }
-//        }
-//
-//        if (thursdayList != null) {
-//            ArrayList<SubjectModel> getThursdayList = new Gson().fromJson(thursdayList, new TypeToken<ArrayList<SubjectModel>>()
-//            {
-//            }.getType());
-//
-//            for (int i = 0; i < getThursdayList.size(); i++) {
-//                Singleton.getInstance().addThuSubject(getThursdayList.get(i));
-//            }
-//        }
-//
-//        if (fridayList != null) {
-//            ArrayList<SubjectModel> getFridayList = new Gson().fromJson(fridayList, new TypeToken<ArrayList<SubjectModel>>()
-//            {
-//            }.getType());
-//
-//            for (int i = 0; i < getFridayList.size(); i++) {
-//                Singleton.getInstance().addFriSubject(getFridayList.get(i));
-//            }
-//        }
-//
-//        if (saturdayList != null) {
-//            ArrayList<SubjectModel> getSaturdayList = new Gson().fromJson(saturdayList, new TypeToken<ArrayList<SubjectModel>>()
-//            {
-//            }.getType());
-//
-//            for (int i = 0; i < getSaturdayList.size(); i++) {
-//                Singleton.getInstance().addSatSubject(getSaturdayList.get(i));
-//            }
-//        }
-//
-//        if (sundayList != null) {
-//            ArrayList<SubjectModel> getSundayList = new Gson().fromJson(sundayList, new TypeToken<ArrayList<SubjectModel>>()
-//            {
-//            }.getType());
-//
-//            for (int i = 0; i < getSundayList.size(); i++) {
-//                Singleton.getInstance().addSunSubject(getSundayList.get(i));
-//            }
-//        }
-//
-//
-////        updateSingletonLists(mondayList);
-////        updateSingletonLists(tuesdayList);
-////        updateSingletonLists(wednesdayList);
-////        updateSingletonLists(thursdayList);
-////        updateSingletonLists(fridayList);
-////        updateSingletonLists(saturdayList);
-////        updateSingletonLists(sundayList);
-//
-//    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        int fabVisibility = fab.getVisibility();
+        int toolbarVisibility = toolbar.getVisibility();
+        int tablayoutVisibility = tabLayout.getVisibility();
+        int saveEdit = edit;
 
+        outState.putInt("fabVisibility", fabVisibility);
+        outState.putInt("toolbarVisibility", toolbarVisibility);
+        outState.putInt("tablayoutVisibility", tablayoutVisibility);
+        outState.putInt("saveEdit", saveEdit);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        frameLayout.bringToFront();
+        fab.setVisibility(savedInstanceState.getInt("fabVisibility"));
+        toolbar.setVisibility(savedInstanceState.getInt("toolbarVisibility"));
+        tabLayout.setVisibility(savedInstanceState.getInt("tablayoutVisibility"));
+        edit = savedInstanceState.getInt("saveEdit");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case R.id.settings_id:
+                Intent openSettings = new Intent(getApplicationContext(), Settings.class);
+                startActivity(openSettings);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public void onBackPressed()
@@ -203,6 +158,7 @@ public class MainActivity extends AppCompatActivity implements Communicator
         toolbar.setVisibility(View.VISIBLE);
         tabLayout.setVisibility(View.VISIBLE);
         fab.setVisibility(View.VISIBLE);
+        edit = 0;
     }
 
     // Fill pagerAdapter with Fragments and Titles
