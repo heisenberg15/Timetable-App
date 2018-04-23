568080ccee98157a8b9ae38fe057b823b070e90b
heisenberg15 <txelidzemaka@yahoo.com>
2018-04-23 18:16:45 +0400

    Settings activity
    Settings activity basic design

--- app/src/main/java/com/example/fergie/timetable/Settings.java
+++ app/src/main/java/com/example/fergie/timetable/Settings.java
@@ -1,15 +1,61 @@
 package com.example.fergie.timetable;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class Settings extends AppCompatActivity
 {
+
+    Toolbar toolbar;
+    Switch switchBtn;
+    TextView weekendDays;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        toolbar = findViewById(R.id.settings_toolbar_id);
+        switchBtn = findViewById(R.id.switcher_id);
+        weekendDays = findViewById(R.id.weekend_days_view_id);
+
+        initToolbar();
+        toggleSwitch();
+
     }
+
+    private void initToolbar()
+    {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void toggleSwitch()
+    {
+        weekendDays.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                switchBtn.toggle();
+            }
+        });
+    }
+
 }
